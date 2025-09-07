6d637c67072bbf85ef5b9a404fcffd5f5f843def
jasonluchn-glitch <jasonluchn@gmail.com>
2025-09-07 23:04:25 +0800

    Added ball object and associated state machine
    增加了足球，创建了足球状态机

---
+++ scenes/ball/ball_state_factory.gd
@@ -0,0 +1,15 @@
+class_name BallStateFactory
+extends Object
+
+var states : Dictionary
+
+func _init() -> void:
+	states = {
+		Ball.State.CARRIED : BallStateCarried,
+		Ball.State.FREEFORM : BallStateFreeform,
+		Ball.State.SHOT : BallStateShot,
+	}
+
+func get_fresh_state(state : Ball.State) -> BallState:
+	assert(states.has(state), "state doesn't exist!")
+	return states.get(state).new()
