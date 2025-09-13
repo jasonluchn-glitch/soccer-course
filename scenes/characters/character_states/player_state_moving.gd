1c6c5ed78a6a2ca5aaf75494673803e95cc4c266
jasonluchn-glitch <jasonluchn@gmail.com>
2025-09-13 12:48:57 +0800

    Added passing state
    增加了传球动作，学习新函数：
    get_overlapping_bodies()
    数组filter
    比较两个距离大小用distance_squared_to()

--- scenes/characters/character_states/player_state_moving.gd
+++ scenes/characters/character_states/player_state_moving.gd
@@ -13,9 +13,14 @@ func _process(_delta: float) -> void:
 func handle_human_movement():
 	var direction := KeyUtils.get_input_vector(player.control_scheme)
 	player.velocity = direction * player.speed
+	if player.velocity != Vector2.ZERO:
+		teammate_detection_area.rotation = player.velocity.angle()
 	
 	if player.has_ball() and KeyUtils.is_action_pressed(player.control_scheme, KeyUtils.Action.SHOOT):
 		transition_state(Player.State.PREPPING_SHOT)
+	
+	if player.has_ball() and KeyUtils.is_action_pressed(player.control_scheme, KeyUtils.Action.PASS):
+		transition_state(Player.State.PASSING)
 		
 	#if player.velocity != Vector2.ZERO and KeyUtils.is_action_pressed(player.control_scheme,KeyUtils.Action.SHOOT):
 		#state_transition_requested.emit(Player.State.TACKLING)
